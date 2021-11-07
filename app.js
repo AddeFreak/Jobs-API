676bfe60ab0f3888e81693801fde3b70aef4e352
Andreas Frick <andreas.frick.toresand@hotmail.com>
2021-11-07 16:13:25 +0100

    swagger docs added

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -32,8 +36,9 @@ app.use(cors());
 app.use(xss());
 
 app.get('/', (req, res) => {
-	res.send('jobs api');
+	res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
